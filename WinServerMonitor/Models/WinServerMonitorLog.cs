de8aeedf90148f953f108f0226d2a7b416b8efc2
decadestory <jerry.core@foxmail.com>
2018-03-24 13:12:13 +0800

    first

---
+++ WinServerMonitor/Models/WinServerMonitorLog.cs
@@ -0,0 +1,34 @@
+﻿using Orm.Son.Mapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinServerMonitor.Models
+{
+    public class WinServerMonitorLog
+    {
+        [Key,Description("编号GUID")]
+        public string Id { get; set; }
+
+        [Description("CPU使用率")]
+        public double ProcessorPercent { get; set; }
+
+        [Description("内存使用率")]
+        public double MemoryPercent { get; set; }
+
+        [Description("IIS实例名")]
+        public string InstanceName { get; set; }
+
+        [Description("IIS实例当前连接数")]
+        public double InstanceConnectionCnt { get; set; }
+
+        [Description("IIS实例最大连接数")]
+        public double InstanceMaxConnectionCnt { get; set; }
+
+        [Description("添加时间")]
+        public DateTime AddTime { get; set; }
+
+    }
+}
---
+++ WinServerMonitor/WinServerMonitorService.cs
@@ -0,0 +1,77 @@
+﻿using Orm.Son.Core;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WinServerMonitor.Models;
+using System.Configuration;
+
+namespace WinServerMonitor
+{
+    public static class WinServerMonitorService
+    {
+        static bool WRITE_TO_DB;
+        static string IIS_INSTANCE;
+        static WinServerMonitorService()
+        {
+            SonFact.Init("conn");
+            WRITE_TO_DB = ConfigurationManager.AppSettings["write_to_db"] == "true" ? true : false;
+            IIS_INSTANCE = ConfigurationManager.AppSettings["iis_instance"] ?? "_Total";
+        }
+
+        public static void Start()
+        {
+            var isOk = SonFact.Cur.CreateTable<WinServerMonitorLog>();
+
+            var list = new List<PerformanceCounter> {
+                 new PerformanceCounter("Processor","% Processor Time","_Total"),
+                 new PerformanceCounter("Memory","% Committed Bytes In Use"),
+                 new PerformanceCounter("Web Service","Current Connections",IIS_INSTANCE),
+                 new PerformanceCounter("Web Service","Maximum Connections",IIS_INSTANCE),
+            };
+
+            Runner(list);
+        }
+
+        public static void Runner(List<PerformanceCounter> counters)
+        {
+            while (true)
+            {
+                var log = new WinServerMonitorLog();
+                foreach (var c in counters)
+                {
+                    var result = c.NextValue();
+                    if (c.CategoryName == "Processor" && c.CounterName == "% Processor Time" && c.InstanceName == "_Total")
+                        log.ProcessorPercent = result;
+                    else if (c.CategoryName == "Memory" && c.CounterName == "% Committed Bytes In Use")
+                        log.MemoryPercent = result;
+                    else if (c.CategoryName == "Web Service" && c.CounterName == "Current Connections" && c.InstanceName == IIS_INSTANCE)
+                        log.InstanceConnectionCnt = result;
+                    else if (c.CategoryName == "Web Service" && c.CounterName == "Maximum Connections" && c.InstanceName == IIS_INSTANCE)
+                    {
+                        log.InstanceName = c.InstanceName;
+                        log.InstanceMaxConnectionCnt = result;
+                    }
+                }
+
+                log.ProcessorPercent = Math.Round(log.ProcessorPercent, 2);
+                log.MemoryPercent = Math.Round(log.MemoryPercent, 2);
+
+                Console.WriteLine(string.Format("CPU:{0}%\t Memory:{1}%\t Iis-cur-conn:{2}\t Iis-max-conn:{3}", log.ProcessorPercent, log.MemoryPercent, log.InstanceConnectionCnt, log.InstanceMaxConnectionCnt));
+                if(WRITE_TO_DB)  AddLog(log);
+                Thread.Sleep(1000);
+            }
+        }
+
+        public static void AddLog(WinServerMonitorLog log)
+        {
+            log.Id = Guid.NewGuid().ToString("N");
+            log.AddTime = DateTime.Now;
+            SonFact.Cur.Insert(log);
+        }
+
+    }
+}
